1c22b8dbf9acd16b40bf8ca0986dc022c676be1e
Bryton Javiérre Herlong <brytboy@Brytons-MacBook-Pro.local>
2022-01-12 18:51:47 -0500

    updating project

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
 /**
- * This is the main entrypoint to your Probot app
  * @param {import('probot').Probot} app
  */
 module.exports = (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+  app.on("repository.created", async (context) => {
+    console.log("Repo created console.log!")
+    required_approvers = 2;
+    console.log(context.payload);
+    
+    const protection_rule = await context.octokit.request("PUT /repos/{owner}/{repo}/branches/{branch}/protection", {
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      branch: context.payload.repository.default_branch,
+      required_status_checks: {
+        strict : true,
+        contexts: []
+      },
+      required_pull_request_reviews: {
+        dismiss_stale_reviews: true,
+        require_code_owner_reviews: true,
+        required_approving_review_count: required_approvers
+      },
+      restrictions: null,
+      enforce_admins: true
     });
-    return context.octokit.issues.createComment(issueComment);
+
+    return context.octokit.issues.create({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name, 
+      title: "New Branch Protection Rule Created!",
+      body: `
+      New branch protection rule has been added to:
+      - Require status checks to pass
+      - Require pull request reviews before merge
+      - Require at least ${required_approvers} approvers for pull requests before merge
+      - Require code owners reviews before pull request merges
+      - Dismiss stale pull request reviews
+      `
+    });
   });
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
